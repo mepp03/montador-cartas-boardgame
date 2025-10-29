09b7b55c8969a45323a62251248a4ebafc895443
Mepp <black.mepp@gmail.com>
2025-10-29 18:37:51 -0300

    Add .nojekyll and fix build config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  base: "/montador-cartas-boardgame/",
+  build: {
+    outDir: "docs", // ← Adicione esta linha
+  },
+});
